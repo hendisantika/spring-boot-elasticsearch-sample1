ab5ea86e654a465199d75b2f8605f211cfd15b73
hendisantika <hendisantika@yahoo.co.id>
2024-12-15 01:15:05 +0700

    Add ImperativeElasticsearchConfig🫸🌀✏️📗🐧🐳⬆

---
+++ src/main/java/id/my/hendisantika/elasticsearchsample1/config/ImperativeElasticsearchConfig.java
@@ -0,0 +1,28 @@
+package id.my.hendisantika.elasticsearchsample1.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.data.elasticsearch.client.ClientConfiguration;
+import org.springframework.data.elasticsearch.client.elc.ElasticsearchConfiguration;
+import org.springframework.data.elasticsearch.repository.config.EnableElasticsearchRepositories;
+
+/**
+ * Created by IntelliJ IDEA.
+ * Project : spring-boot-elasticsearch-sample1
+ * User: hendisantika
+ * Email: hendisantika@gmail.com
+ * Telegram : @hendisantika34
+ * Date: 15/12/24
+ * Time: 01.14
+ * To change this template use File | Settings | File Templates.
+ */
+@Configuration
+@EnableElasticsearchRepositories(basePackages = "id.my.hendisantika.elasticsearchsample1")
+public class ImperativeElasticsearchConfig extends ElasticsearchConfiguration {
+
+    @Override
+    public ClientConfiguration clientConfiguration() {
+        return ClientConfiguration.builder()
+                .connectedTo("localhost:9200")
+                .build();
+    }
+}
