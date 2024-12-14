4e84819221f37e664a18878e32f87dd581502a50
hendisantika <hendisantika@yahoo.co.id>
2024-12-15 01:36:11 +0700

    feat: Add elasticsearchContainer🫸🌀✏️📗🐧🐳⬆

--- src/test/java/id/my/hendisantika/elasticsearchsample1/SpringBootElasticsearchSample1ApplicationTests.java
+++ src/test/java/id/my/hendisantika/elasticsearchsample1/SpringBootElasticsearchSample1ApplicationTests.java
@@ -1,8 +1,9 @@
 package id.my.hendisantika.elasticsearchsample1;
 
-import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.TestInstance;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.testcontainers.elasticsearch.ElasticsearchContainer;
+import org.testcontainers.junit.jupiter.Container;
 import org.testcontainers.junit.jupiter.Testcontainers;
 
 @SpringBootTest
@@ -10,8 +11,13 @@ import org.testcontainers.junit.jupiter.Testcontainers;
 @TestInstance(TestInstance.Lifecycle.PER_CLASS)
 class SpringBootElasticsearchSample1ApplicationTests {
 
-    @Test
-    void contextLoads() {
-    }
+    @Container
+    public static ElasticsearchContainer elasticsearchContainer = new ElasticsearchContainer(
+            "docker.elastic.co/elasticsearch/elasticsearch:8.16.1")
+            .withExposedPorts(9200)
+            .withEnv("discovery.type", "single-node")
+            .withEnv("xpack.security.enabled", "false");
+    private EmployeeRepositoryService employeeRepositoryService;
+
 
 }
