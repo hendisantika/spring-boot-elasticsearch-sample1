cc39edf65ca398be5560a5e6ea17f4778471c153
hendisantika <hendisantika@yahoo.co.id>
2024-12-15 01:35:28 +0700

    feat: Add SpringBootElasticsearchSample1ApplicationTests🫸🌀✏️📗🐧🐳⬆

--- src/test/java/id/my/hendisantika/elasticsearchsample1/SpringBootElasticsearchSample1ApplicationTests.java
+++ src/test/java/id/my/hendisantika/elasticsearchsample1/SpringBootElasticsearchSample1ApplicationTests.java
@@ -1,9 +1,13 @@
 package id.my.hendisantika.elasticsearchsample1;
 
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.TestInstance;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.testcontainers.junit.jupiter.Testcontainers;
 
 @SpringBootTest
+@Testcontainers
+@TestInstance(TestInstance.Lifecycle.PER_CLASS)
 class SpringBootElasticsearchSample1ApplicationTests {
 
     @Test
